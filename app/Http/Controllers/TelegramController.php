f2c05e636e6a5b36e0e92d70f5d9f551e780a125
isaac-chinonso <isaacchinonsogift@gmail.com>
2020-09-16 04:28:08 +0100

    updated controller

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -1,7 +1,7 @@
 <?php
-
+ 
 namespace App\Http\Controllers;
-
+ 
 use App\Telegram;
 use Carbon\Carbon;
 use coinmarketcap\api\CoinMarketCap;
@@ -29,7 +29,7 @@ class TelegramController extends Controller
  
     public function setWebHook()
     {
-        $url = 'https://github.com/isaac-chinonso/cryptoPAlertBot' . env('TELEGRAM_BOT_TOKEN') . '/webhook';
+        $url = 'https://applicationdomain.com/' . env('TELEGRAM_BOT_TOKEN') . '/webhook';
         $response = $this->telegram->setWebhook(['url' => $url]);
  
         return $response == true ? redirect()->back() : dd($response);
